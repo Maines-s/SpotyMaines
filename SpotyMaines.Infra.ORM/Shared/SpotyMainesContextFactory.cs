7fcd504e589097cc1d97523de5b20bc4f2a20093
Felipe Maines <felipemaines123@gmail.com>
2023-12-10 17:36:33 -0300

    faz a criacao do orm e suas configuracoes

---
+++ SpotyMaines.Infra.ORM/Shared/SpotyMainesContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotyMaines.Infra.ORM.Shared
+{
+    public class SpotyMainesContextFactory : IDesignTimeDbContextFactory<SpotyMainesDbContext>
+    {
+        public SpotyMainesDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SpotyMainesDbContext>();
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = config.GetConnectionString("SqlServer");
+
+            builder.UseSqlServer(connectionString);
+
+            return new SpotyMainesDbContext(builder.Options);
+        }
+    }
+}
---
+++ SpotyMaines.Infra.ORM/Shared/SpotyMainesDbContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SpotyMaines.Domain.AutenticationModule;
+using SpotyMaines.Domain.FriendModule;
+using SpotyMaines.Domain.ListenerModule;
+using SpotyMaines.Domain.MusicsModule;
+using SpotyMaines.Domain.PlayListModule;
+using SpotyMaines.Domain.RoomModule;
+using SpotyMaines.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotyMaines.Infra.ORM.Shared
+{
+    public class SpotyMainesDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        private Guid userId;
+
+        public SpotyMainesDbContext(DbContextOptions opt, ITenantProvider tenantProvider = null) : base(opt)
+        {
+            if (tenantProvider != null)
+                userId = tenantProvider.UserId;
+        }
+
+        public async Task<bool> SaveData()
+        {
+            int afectedRegister = await SaveChangesAsync();
+
+            return afectedRegister > 0;
+        }
+
+        public void UndoChanges()
+        {
+            var afectedRegister = ChangeTracker.Entries()
+            .Where(e => e.State != EntityState.Unchanged)
+            .ToList();
+
+            foreach (var register in afectedRegister)
+            {
+                switch (register.State)
+                {
+                    case EntityState.Added:
+                        register.State = EntityState.Detached;
+                        break;
+
+                    case EntityState.Deleted:
+                        register.State = EntityState.Unchanged;
+                        break;
+
+                    case EntityState.Modified:
+                        register.State = EntityState.Unchanged;
+                        register.CurrentValues.SetValues(register.OriginalValues);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(SpotyMainesDbContext).Assembly);
+
+            builder.Entity<Friend>().HasQueryFilter(x => x.UserId == userId);
+            builder.Entity<Listener>().HasQueryFilter(x => x.UserId == userId);
+            builder.Entity<Music>().HasQueryFilter(x => x.UserId == userId);
+            builder.Entity<PlayList>().HasQueryFilter(x => x.UserId == userId);
+            builder.Entity<Room>().HasQueryFilter(x => x.UserId == userId);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
