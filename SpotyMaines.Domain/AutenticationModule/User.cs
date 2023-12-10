1fed14889412bc1f714267800d7963650ed941c9
Felipe Maines <felipemaines123@gmail.com>
2023-12-10 16:51:00 -0300

    feat: cria o esqueleto do dominio

---
+++ SpotyMaines.Domain/AutenticationModule/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taikandi;
+
+namespace SpotyMaines.Domain.AutenticationModule
+{
+    public class User : IdentityUser<Guid>
+    {
+        public User()
+        {
+            Id = SequentialGuid.NewGuid();
+        }
+
+        public string Name { get; set; }
+    }
+}
